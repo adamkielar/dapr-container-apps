3cbfadde55231799fe6d1c0572a28f0c1658d13f
Adam Kielar <adam.kielar@netguru.com>
2022-09-22 20:20:01 +0200

    Python receiver 28

--- python-receiver/app/main.py
+++ python-receiver/app/main.py
@@ -30,15 +30,15 @@ def health_check():
 
 @app.post("/planets")
 async def save_planet(planet: Planet = Body(...)) -> int:
-    planet_data = json.dumps([planet.dict()])
+    planet_data = json.dumps(planet.dict())
 
     async with ClientSession() as session:
         response = await session.post(
-            url='http://localhost:3500/v1.0/state/statestore',
+            url='http://localhost:3500/v1.0/state/statestore?metadata.contentType=application/json',
             json=planet_data
         )
         logging.info(f'Saving planets: {planet_data}')
-    return response.status_code
+    return response.status
 
 
 @app.get("/planets/{planet_name}")
