1ef7beb8271ca32fe5dd830b9a39270f1cc28640
Adam Kielar <adam.kielar@netguru.com>
2022-09-22 20:00:47 +0200

    Python receiver 27

--- python-receiver/app/main.py
+++ python-receiver/app/main.py
@@ -1,7 +1,11 @@
+import json
 import logging
 import requests
+from typing import Dict
 
-from fastapi import Body, FastAPI
+from aiohttp import ClientSession
+from fastapi import Body
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 logging.basicConfig(level=logging.INFO)
@@ -9,8 +13,14 @@ logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 
 
-class Order(BaseModel):
-    order_id: str
+class PlanetDetails(BaseModel):
+    kind: str
+    status: str
+
+
+class Planet(BaseModel):
+    key: str
+    value: PlanetDetails
 
 
 @app.get("/health")
@@ -18,27 +28,38 @@ def health_check():
     return {"status": "Healthy"}
 
 
-@app.post("/orders")
-async def save_order():
-    orderId = '1'
-    order = {'orderId': orderId}
-    state = [{
-      'key': orderId,
-      'value': order
-    }]
-    result = requests.post(
+@app.post("/planets")
+async def save_planet(planet: Planet = Body(...)) -> int:
+    planet_data = json.dumps([planet.dict()])
+
+    async with ClientSession() as session:
+        response = await session.post(
             url='http://localhost:3500/v1.0/state/statestore',
-            json=state
+            json=planet_data
         )
-    logging.info(f'Saving order {order}')
-    return result.json()
+        logging.info(f'Saving planets: {planet_data}')
+    return response.status_code
+# @app.post("/orders")
+# async def save_order(order):
+#     orderId = '1'
+#     order = {'orderId': orderId}
+#     state = [{
+#       'key': orderId,
+#       'value': order
+#     }]
+#     result = requests.post(
+#             url='http://localhost:3500/v1.0/state/statestore',
+#             json=state
+#         )
+#     logging.info(f'Saving order {order}')
+#     return result.status_code
 
 
-@app.get("/orders")
-async def get_order():
-    if result := requests.get(
-            url='http://localhost:3500/v1.0/state/statestore/1'
-    ):
-        logging.info(f'Retrieved order {result.json()}')
-        return result.json()
-    return result.status_code
+# @app.get("/orders")
+# async def get_order():
+#     if result := requests.get(
+#             url='http://localhost:3500/v1.0/state/statestore/1'
+#     ):
+#         logging.info(f'Retrieved order {result.json()}')
+#         return result.json()
+#     return result.status_code
