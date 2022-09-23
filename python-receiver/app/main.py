ddd71910f8af91c9e6f9826a4479458e32729051
Adam Kielar <adam.kielar@netguru.com>
2022-09-23 10:44:48 +0200

    Python receiver 37

--- python-receiver/app/main.py
+++ python-receiver/app/main.py
@@ -1,6 +1,8 @@
 import logging
 from typing import Dict
 
+from dapr.clients import DaprClient
+from dapr.clients.grpc.client import DaprResponse
 from fastapi import FastAPI
 from httpx import AsyncClient
 from pydantic import BaseModel
@@ -8,6 +10,9 @@ from pydantic import BaseModel
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
+
+
+DAPR_STORE_NAME = "statestore"
 
 
 class PlanetDetails(BaseModel):
@@ -31,7 +36,7 @@ async def save_planet(planet: Planet) -> int:
     data = [planet.dict()]
     async with AsyncClient() as client:
         response = await client.post(
-            url='http://localhost:3500/v1.0/state/statestore',
+            url=f'http://localhost:3500/v1.0/state/{DAPR_STORE_NAME}',
             json=data
         )
     logging.info(f'Saving planet: {data}')
@@ -42,7 +47,31 @@ async def save_planet(planet: Planet) -> int:
 async def get_planet(planet_id: str) -> Dict:
     async with AsyncClient() as client:
         response = await client.get(
-            url=f'http://localhost:3500/v1.0/state/statestore/{planet_id}'
+            url=f'http://localhost:3500/v1.0/state/{DAPR_STORE_NAME}/{planet_id}'
         )
         logging.info(f'Retrieve planet: {planet_id}')
     return response.json()
+
+
+@app.post("/sdk/planets")
+async def save_planet(planet: Planet) -> DaprResponse:
+    data = planet.dict()
+    with DaprClient() as client:
+        response = client.save_state(
+            store_name=DAPR_STORE_NAME,
+            key=data.get('key'),
+            value=data.get('value')
+        )
+    logging.info(f'Saving planet: {data}')
+    return response
+
+
+@app.get("/sdk/planets/{planet_id}")
+async def get_planet(planet_id: str) -> str:
+    with DaprClient() as client:
+        response = client.get_state(
+            store_name=DAPR_STORE_NAME,
+            key=planet_id
+        )
+        logging.info(f'Retrieve planet: {planet_id}')
+    return str(response.data)
